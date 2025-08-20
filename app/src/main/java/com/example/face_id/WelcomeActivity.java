4be5a09e927c2671c2af8a3848ceecae58f67b3b
manhduc <2251172281@e.tlu.edu.vn>
2025-08-20 17:03:15 +0700

    Demo lại giao diện login

--- app/src/main/java/com/example/face_id/WelcomeActivity.java
+++ app/src/main/java/com/example/face_id/WelcomeActivity.java
@@ -2,14 +2,9 @@ package com.example.face_id;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityOptionsCompat;
-import androidx.core.util.Pair;
-
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -18,30 +13,13 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-
-        ImageView bgPattern = findViewById(R.id.bgPattern);
-        View background = findViewById(R.id.activity_welcome);
+        setContentView(R.layout.activity_welcome); // layout của bạn
 
         btnContinue = findViewById(R.id.btnContinue);
         btnContinue.setOnClickListener(v -> {
             Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
-
-            Pair<View, String> p1 = Pair.create(bgPattern, "bgPattern");
-            Pair<View, String> p2 = Pair.create(background, "background_trans");
-
-            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(WelcomeActivity.this, p1, p2);
-            Bundle bundle = options.toBundle();
-
-            if (bundle != null) {
-                startActivity(intent, bundle);
-            } else {
-                startActivity(intent);
-            }
-
-            finish();
+            startActivity(intent);
+             finish();
         });
     }
 }
-
-
